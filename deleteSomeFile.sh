f538f3e7c7002ccf412225980ee846fff6a93dea
rafifazzaki <59862154+rafifazzaki@users.noreply.github.com>
2024-01-12 14:20:54 +0700

    added deleteSomeFile
    delete some file for ubuntu

---
+++ deleteSomeFile.sh
@@ -0,0 +1,5 @@
+rm Dicoding_Belajar-Membuat-Aplikasi-Back-End-untuk-Pemula/notes-app-back-end/notes-api-webserver.pem
+
+rm Dicoding_Belajar-Membuat-Aplikasi-Back-End-untuk-Pemula/notes-app-back-end/IAMrafifazzaki_credentials.csv
+
+rm -r Dicoding_Belajar-Membuat-Aplikasi-Back-End-untuk-Pemula/notes-app-back-end/node_modules
